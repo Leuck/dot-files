89e3a366c45972c98ce588fdafe5f5abe41afbb2
Ricardo Leuck <fred.leuck@gmail.com>
2017-07-25 23:19:28 -0300

    Added listing option to deflater script.

--- .bashrc
+++ .bashrc
@@ -9,7 +9,7 @@ alias xrandrhdmi='xrandr --output eDP1 --auto --output HDMI1 --auto --above eDP1
 alias xrandright='xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1 --set "Broadcast RGB" "Full"'
 alias xrandroff='xrandr --output eDP1 --auto --output HDMI1 --off'
 alias webcam='mpv tv:// --tv-driver=v4l2 --tv-device=/dev/video0'
-alias deflate='~/git/dot-files/.tarhelp.sh'
+alias deflatar='~/git/dot-files/.tarhelp.sh'
 export EDITOR=/usr/bin/vim
 export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORMTHEME=qt5ct
--- .tarhelp.sh
+++ .tarhelp.sh
@@ -1,30 +1,91 @@
 #!/usr/bin/bash
 # tar helper
 
-filename=$(basename "$1")
-extension="${filename##*.}"
+ARGC=$#
+USAGE="
+Usage:
 
-case "$extension" in
-    "tar")
-        echo "Deflating $filename"
-        tar -xvf $filename
-        ;;
-    "gz")
-        echo "Deflating $filename"
-        tar -zxvf $filename
-        ;;
-    "bz2")
-        echo "Deflating $filename"
-        tar -jxvf $filename
-        ;;
-    "rar")
-        echo "Deflating $filename"
-        unrar e $filename
-        ;;
-    "zip")
-        echo "Deflating $filename"
-        unzip $filename
-        ;;
-    *)
-        echo "$filename: No deflate rule for .$extension"
-esac
+Extract contents of file FILE:
+
+    deflatar FILE
+
+List contents of file FILE:
+
+    deflatar -l FILE
+"
+echo $#
+if [ $ARGC -eq 1 ]
+then
+    filename=$(basename "$1")
+    if [ -f $filename ]
+        then
+        extension="${filename##*.}"
+        case "$extension" in
+            "tar")
+                echo "Deflating $filename"
+                tar -xvf $filename
+                ;;
+            "gz")
+                echo "Deflating $filename"
+                tar -zxvf $filename
+                ;;
+            "bz2")
+                echo "Deflating $filename"
+                tar -jxvf $filename
+                ;;
+            "rar")
+                echo "Deflating $filename"
+                unrar e $filename
+                ;;
+            "zip")
+                echo "Deflating $filename"
+                unzip $filename
+                ;;
+            *)
+                echo "$filename: No deflate rule for .$extension"
+                ;;
+        esac
+    else
+        echo "$filename: Not a valid file."
+    fi
+elif [ $ARGC -eq 2 ]
+then
+    while getopts :l: option
+    do
+        case "${option}" in
+            l)
+                filename=$(basename "${OPTARG}")
+                extension="${filename##*.}"
+                case "$extension" in
+                    "tar")
+                        echo "Listing $filename"
+                        tar -tvf $filename
+                        ;;
+                    "gz")
+                        echo "Listing $filename"
+                        tar -ztvf $filename
+                        ;;
+                    "bz2")
+                        echo "Listing $filename"
+                        tar -jtvf $filename
+                        ;;
+                    "rar")
+                        echo "Listing $filename"
+                        unrar l $filename
+                        ;;
+                    "zip")
+                        echo "Listing $filename"
+                        unzip -l $filename
+                        ;;
+                    *)
+                        echo "$filename: No listing rule for .$extension"
+                        ;;
+                esac
+                ;;
+            \?) echo "Invalid switch." ;;
+        esac
+    done
+else
+    echo "$USAGE"
+fi
+
